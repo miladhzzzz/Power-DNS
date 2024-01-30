89c9302dd7f325d790b2afe9f9ed90d966d8ee57
milx <david.hacker09@gmail.com>
2024-01-30 20:35:55 +0330

    adding api structure

---
+++ internal/api/server.go
@@ -0,0 +1,15 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+func StartGinAPI() {
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+}

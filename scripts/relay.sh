6f8938f46ac6bb09f1702bed2702393c314b647d
milx <david.hacker09@gmail.com>
2024-01-31 18:24:15 +0330

    improved script

--- scripts/relay.sh
+++ scripts/relay.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-# we want to run the dns server as a relay on a remote host using docker and argo
+# Check if Docker is installed
+if! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Installing it now..."
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+fi
 
-# to start the process we first build the docker image
+# Build the Docker image
+echo "Building docker image ...."
+cd..
+if! docker build -t power-dns:latest.
+then
+    echo "Failed to build Docker image. Aborting."
+    exit 1
+fi
 
-cd ..
+# Run the Docker container
+echo "Running power-dns Container ...."
+if! docker run --name power-dns -p 8000:8000 -d power-dns:latest
+then
+    echo "Failed to run Docker container. Aborting."
+    exit 1
+fi
 
-docker build -t power-dns:latest .
+# Tell Argo to listen on localhost:8000
+echo "Running Argo tunnel ..."
+if! nohup cfd tunnel --url localhost:8000 --edge-ip-version auto --no-autoupdate -protocol http2 >>dns.log &
+then
+    echo "Failed to start Argo tunnel. Aborting."
+    exit 1
+fi
 
-# next we need to run the container
-
-docker run --name power-dns -p -d 8000:8000 5335:5335 power-dns:latest
-
-# then we need to tell argo to start listening on localhost:8000
-
-nohup cfd tunnel --url localhost:8000 --edge-ip-version auto --no-autoupdate -protocol http2 >>dns.log &
-
-# then we wait 5 seconds and cat the dns.log and exit
+# Wait for 5 seconds and cat the log file
+echo "waiting for Tuneel URL ..."
 sleep 5
-
 cat dns.log

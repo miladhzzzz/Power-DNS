fc9e2216bcad3151d869177a434f541f2068bda1
milx <david.hacker09@gmail.com>
2024-01-31 15:04:01 +0330

    refactored api module

--- internal/api/routes/dns.routes.go
+++ internal/api/routes/dns.routes.go
@@ -1,36 +1,26 @@
 package routes
 
-//
-//import (
-//	"github.com/gin-gonic/gin"
-//
-//	"github.com/miladhzzzz/power-dns/internal/api/controllers"
-//)
-//
-//var (
-//	ctx *gin.Context
-//)
-//
-//type DNSRouteController struct {
-//	dnsController controllers.dnsController
-//}
-//
-//func NewDNSRouteController(authController controllers.AuthController) DNSRouteController {
-//	return AuthRouteController{authController}
-//}
-//
-//func (rc *DNSRouteController) DNSRoute(rg *gin.RouterGroup) {
-//	router := rg.Group("/dns")
-//
-//	router.GET("/login", rc.authController.LoginHandler())
-//	router.POST("/cli", rc.authController.Cli())
-//
-//	private := router.Group("")
-//
-//	private.Use(rc.authController.Auth())
-//
-//	private.GET("/", func(context *gin.Context) {
-//
-//	})
-//
-//}
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/miladhzzzz/power-dns/internal/api/controllers"
+)
+
+var ctx *gin.Context
+
+type DNSRouteController struct {
+	dnsController controllers.DNSController
+}
+
+func NewDNSRouteController(DNSController controllers.DNSController) DNSRouteController {
+	return DNSRouteController{DNSController}
+}
+
+func (dc *DNSRouteController) DNSRoute(rg *gin.RouterGroup) {
+	router := rg.Group("/dns")
+
+	router.GET("/Query", dc.dnsController.Query())
+
+	// router.POST("/cli", rc.authController.Cli())
+
+}
--- internal/api/server.go
+++ internal/api/server.go
@@ -1,15 +1,49 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	"github.com/miladhzzzz/power-dns/internal/api/controllers"
+	"github.com/miladhzzzz/power-dns/internal/api/routes"
+)
+
+var (
+	DNSController      controllers.DNSController
+	DNSRouteController routes.DNSRouteController
+
+	server *gin.Engine
+)
+
+func init() {
+	DNSController = controllers.NewDNSController()
+	DNSRouteController = routes.NewDNSRouteController(DNSController)
+
+	logFile, _ := os.Create("DNS-HTTP.log")
+
+	server = gin.Default()
+
+	server.Use(gin.LoggerWithWriter(logFile))
+}
 
 func StartGinAPI() {
-	r := gin.Default()
+	r := server.Group("")
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:8000"}
+	corsConfig.AllowCredentials = true
+
+	server.Use(cors.New(corsConfig))
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "pong!"})
 	})
 
-	r.Run(":8000")
+	DNSRouteController.DNSRoute(r)
+
+	log.Fatal(server.Run(":8000"))
 }
